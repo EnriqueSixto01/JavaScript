ff67b4db2aa5ee6ad823e79321f16c7f0cf93883
EnriqueSixto <cv4713@innovaccion.mx>
2022-03-11 19:10:16 -0600

    variables

--- Variables.js
+++ Variables.js
@@ -1,14 +1,27 @@
+/***********************************Declarando Variables y asignandoles un valor********************/
+var myName =  "Enrique";
+var numero = 26;
+var booleano = true;
+
+/***********************************Assigning the Value of one variable to another******************/
+var x;
+x = 10;
+var y;
+y = x;
+console.log(y);
 /***********************************Understanding Uninitialized Variables***************************/
 // Only change code below this line
-var a = 5;
+var a;
 var b = 10;
 var c = "I am";
-// Only change code above this line ***
+var d;
+// Only change code above this line 
 
 a = a + 1;
 b = b + 5;
 c = c + " String!";
-
+d = d + " Variable"
 console.log(a);
 console.log(b);
 console.log(c);
+console.log(d);
